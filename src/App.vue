7bfb45e06309043f9fb33e665e4741a5ddc861b5
wemali05 <alfredwemali05@gmail.com>
2020-02-21 16:06:08 +0300

    animating route transition

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@
                 <h1>Routing</h1>
                 <hr>
                <router-view name="header-top"></router-view>
-               <router-view></router-view>
-               <router-view  name="header-bottom"></router-view>
+               <transition name="slide" mode="out-in">
+                 <router-view></router-view>
+               </transition>
+              <router-view  name="header-bottom"></router-view>
             </div>
         </div>
     </div>
@@ -22,4 +24,36 @@ import Header from './components/Header.vue';
 </script>
 
 <style>
+   .slide-enter-active{
+        animation:  slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+    .slide-leave-active{
+        animation:  slide-out 1s ease-out forwards;
+         transition: opacity 1s;
+         opacity: 0;
+         position: absolute;
+    }
+
+    .slide-move{
+        transition: transform 1s;
+    }
+
+    @keyframes slide-in{
+       from {
+             transform: translateY(30px);
+       }
+       to {
+             transform: translateY(0);
+       }
+    }
+
+    @keyframes slide-out {
+        from{
+             transform: translateY(0);
+        }
+        to {
+               transform: translateY(30px);
+        }
+    }
 </style>
