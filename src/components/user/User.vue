fad6c6a55ad1e7f7bacedb1e5ef0585cd39420df
wemali05 <alfredwemali05@gmail.com>
2020-02-21 13:16:46 +0300

    navigate from code

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+       <h1>The User Page</h1>
+       <hr>
+       <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    </div>
+  </template>
 
-</template>
+  <script>
+  export default {
+      methods: {
+          navigateToHome(){
+              this.$router.push('/')
+          }
+      }
+  }
+  </script>
